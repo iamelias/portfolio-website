c050be52046e113fdc786261f3ef2340b905b337
Elias Hall <elias.hall@gmail.com>
2020-10-25 12:38:53 -0500

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,39 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import './Navbar.css';
+import { Button } from './Button';
 
 function Navbar() {
   const [click, setClick] = useState(false);
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
+  const [button, setButton] = useState(true);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    }
+    else {
+      setButton(true);
+    }
+  };
+
+  window.addEventListener('resize', showButton);
+
   return (
     <>
       <nav className = 'navbar'>
         <div className='navbar-container'>
-          <Link to="/" className="navbar-logo">
+          <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
             iamElias <i className='fas fa-atom' />
           </Link>
         <div className = 'menu-icon' onClick={handleClick}>
           <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
-          //<i class="fas fa-bars"></i>
-          //<i class="fas fa-atom"></i>
         </div>
         <ul className={click ? 'nav-menu active' : 'nav-menu'}>
           <li className='nav-item'>
-            <Link to='/Home' className='nav-links' onClick={closeMobileMenu}>
-              Home
+            <Link to='/Projects' className='nav-links' onClick={closeMobileMenu}>
+              Projects
             </Link>
           </li>
           <li className='nav-item'>
@@ -29,8 +42,8 @@ function Navbar() {
             </Link>
           </li>
           <li className='nav-item'>
-            <Link to='/Contact' className='nav-links' onClick={closeMobileMenu}>
-              Contact
+            <Link to='/Policy' className='nav-links' onClick={closeMobileMenu}>
+              Privacy Policy
             </Link>
           </li>
       </ul>
@@ -40,4 +53,4 @@ function Navbar() {
   );
 }
 
-export default Navbar
+export default Navbar;
