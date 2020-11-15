2d3b17addb3c8080894430a5bfe0f60578a83150
Elias Hall <elias.hall@gmail.com>
2020-11-14 18:50:02 -0600

    added privacy policy policy page

--- src/components/pages/PrivacyPolicy.js
+++ src/components/pages/PrivacyPolicy.js
@@ -4,14 +4,7 @@ import PrivPolSection from '../PrivPolSection';
 
 function PrivacyPolicy() {
   return (<div className='background'>
-
-    <div class='flex-container'>
-
-      <div class='flex-child'>
         <PrivPolSection/>
-      </div>
-      <div class='flex-child'></div>
-    </div>
   </div>);
 }
 
