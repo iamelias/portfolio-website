83a4652acc383b38ac3afddc3b2fb5c6a185e563
Elias Hall <elias.hall@gmail.com>
2020-11-23 23:24:11 -0600

    fixed mobile and landscape view, background-color in mobile and stretch

--- src/components/IntroSection.js
+++ src/components/IntroSection.js
@@ -9,13 +9,13 @@ function IntroSection() {
       <div className='intro-section'>
         <div className='image'></div>
         <div className='text-section'>
+          <h3>iOS Developer | Product Designer</h3>
           <p>
-            Hi, my name is Elias... I am an iOS Developer, computer science graduate, and Udacity iOS nanodegree graduate. The images below show my projects. ServingsUp and BinDecHex are currently available for free in the Apple App Store. For usage screenshots and code, tap on the image, or GitHub link in the header. A little bit more about me… I enjoy designing UI, and Sketch is my tool of choice. I think UX is very important as well and try to think about my apps as a new user, not just the developer. To keep up with me, my twitter profile is linked in the header. Thanks.
+            Hi, my name is Elias H., This is my online portfolio. Feel free to check out my projects and social media links in the header. I enjoy designing and coding useful and inspiring apps for myself and others.
           </p>
         </div>
-
         <div className='contact-section'>
-          <p>eliasdeveloper1@gmail.com</p>
+          <p> eliasdeveloper1@gmail.com</p>
         </div>
       </div>
     </div>
